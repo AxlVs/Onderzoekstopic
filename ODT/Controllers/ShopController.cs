fb7a25feaacacbeeba45e3e414bcb100c13f0bec
Axl <vansteenacker.axl@gmail.com>
2019-01-29 11:58:57 +0100

    201901

--- ODT/Controllers/ShopController.cs
+++ ODT/Controllers/ShopController.cs
@@ -15,7 +15,7 @@ namespace ODT.Controllers
 
     public IActionResult Index()
     {
-      ViewData["winkelTitel"] = _localizer["winkelTitel"];
+      ViewData["myTitle"] = _localizer["winkelTitel"];
       return View();
     }
   }
