13ca5f49b3600e6928c8e1918272954bb44ceac0
Axl <vansteenacker.axl@gmail.com>
2019-01-29 10:45:51 +0100

    Eerste upload

--- ODT/Controllers/ShopController.cs
+++ ODT/Controllers/ShopController.cs
@@ -1,12 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
 
 namespace ODT.Controllers
 {
   public class ShopController : Controller
   {
-    // GET
+    
+    private readonly IStringLocalizer<HomeController> _localizer;
+
+    public ShopController(IStringLocalizer<HomeController> localizer)
+    {
+      _localizer = localizer;
+    }
+
     public IActionResult Index()
     {
+      ViewData["MyTitle"] = _localizer["The localised title of my app!"];
       return View();
     }
   }
