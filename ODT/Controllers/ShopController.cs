8a82280e97060c35d233447196fec6ddfa59e85a
Axl <vansteenacker.axl@gmail.com>
2019-02-05 08:57:57 +0100

    20190205_ Database toegevoegd

--- ODT/Controllers/ShopController.cs
+++ ODT/Controllers/ShopController.cs
@@ -1,11 +1,16 @@
+using System.Collections.Generic;
+using System.Linq;
+using DAL.EF;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Localization;
+using ODT.Models;
 
 namespace ODT.Controllers
 {
   public class ShopController : Controller
   {
-    
+    private VlaggenRepository repo = new VlaggenRepository();
     private readonly IStringLocalizer<ShopController> _localizer;
 
     public ShopController(IStringLocalizer<ShopController> localizer)
@@ -15,8 +20,13 @@ namespace ODT.Controllers
 
     public IActionResult Index()
     {
+      // return correct language title
       ViewData["controllerTitle"] = _localizer["winkelTitel"];
-      return View();
+      
+      // return data
+      List<Vlag> vlaggen = repo.ReadVlaggen().ToList();
+      
+      return View(vlaggen);
     }
   }
 }
