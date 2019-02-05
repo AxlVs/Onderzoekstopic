3d7bb84eb36e00e9cec3875eb6743efbf2f2b429
Axl <vansteenacker.axl@gmail.com>
2019-02-05 10:10:02 +0100

    20190205_ 10:09

--- ODT/Controllers/ShopController.cs
+++ ODT/Controllers/ShopController.cs
@@ -25,8 +25,31 @@ namespace ODT.Controllers
       
       // return data
       List<Vlag> vlaggen = repo.ReadVlaggen().ToList();
+      
+      // Caching
+      
+      ObjectCache cache = MemoryCache.Default;  
+      string fileContents = cache["filecontents"] as string;  
 
-      ViewBag.counter = 0;
+      if (fileContents == null)  
+      {  
+        CacheItemPolicy policy = new CacheItemPolicy();  
+
+        List<string> filePaths = new List<string>();  
+        filePaths.Add("c:\\cache\\example.txt");  
+
+        policy.ChangeMonitors.Add(new   
+          HostFileChangeMonitor(filePaths));  
+
+        // Fetch the file contents.  
+        fileContents =   
+          File.ReadAllText("c:\\cache\\example.txt");  
+
+        cache.Set("filecontents", fileContents, policy);  
+      }  
+
+      Label1.Text = fileContents;  
+    }  
       
       return View(vlaggen);
     }
